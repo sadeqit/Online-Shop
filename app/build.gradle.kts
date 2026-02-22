6a902ab3b091b25a4ab33fea7699b097c0ee60e7
sadegh-it <sadeghassassin@gmail.com>
2026-02-22 21:17:35 +0330

    chore: configure project with .gitignore, assets, and custom fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation(libs.androidx.work)
     implementation(libs.okhttp)
 
+
+
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     implementation(libs.androidx.compose.ui.text)
